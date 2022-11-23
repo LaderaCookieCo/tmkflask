dda452b68b4c73a75b882f11d732988db5610d35
tmk2-0 <mousseau.david@outlook.com>
2022-11-22 18:34:18 -0800

    added logic, added template inheritance

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,10 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html", title = "hello sayer", user = {"username": "David"})
+    posts = [
+        {"user": {"username": "Xx_potato_eater_xX}"},
+        "body": "are you a potato"},
+        {"user": {"username": "Former U.S. President Barack Obama"},
+        "body": "My fellow Americans, let me be clear: The short form of racecar driver is not a racist."}
+    ]
+    return render_template("index.html", title = "Welcome to Bacefook", user = {"username": "David"}, posts = posts)
