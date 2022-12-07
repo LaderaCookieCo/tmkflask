94c5734d71218ae65f10b724fb29f2dfc55917be
tmk2-0 <mousseau.david@outlook.com>
2022-12-06 18:26:34 -0800

    finished login facade

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route("/index")
@@ -18,5 +18,6 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'login requested for user {form.username.data}')
-        return redirect('/')
+        return redirect(url_for("index"))
     return render_template('login.html', title = 'Sign In', form = form)
+    
